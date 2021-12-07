22cc0bc157490c3bfbb93fb5329cdcd077e4d650
mttalYangin <patsoluekmek3@gmail.com>
2021-12-07 22:02:05 +0300

    eklendi

---
+++ app.py
@@ -0,0 +1,13 @@
+import time
+
+def alarm():
+    print('zaman doldu!')
+while True:
+    
+    saat = (time.strftime("%X"))
+    print(saat)
+    time.sleep(1)
+    
+    if saat == "21:27:00":
+        alarm()
+        break
